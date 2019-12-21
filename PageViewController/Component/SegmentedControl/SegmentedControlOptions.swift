3b9c807668835ad4ec0e704a5c7cf6b998140651
Kam <>
2019-12-21 21:24:26 +0800

    add SegmentedControlOptions to example app target

--- PageViewController/Component/SegmentedControl/SegmentedControlOptions.swift
+++ PageViewController/Component/SegmentedControl/SegmentedControlOptions.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-public struct SegmentedControlOptions {
+open class SegmentedControlOptions {
     var enableSegmentControl: Bool
     var segmentedTitles: [String]
     var selectedTitleColors: [UIColor]
@@ -17,9 +17,9 @@ public struct SegmentedControlOptions {
     var hasRedDot: [Bool]?
     var segmentButtonFontSize: CGFloat
     var navigateToTabIndex: Int
-    var segmentButtonWidth: Int?
+    var isDynamicTabWidth: Bool
     
-    init(enableSegmentControl: Bool = true, segmentedTitles: [String]? = ["Red", "Black"], selectedTitleColors: [UIColor]? = [UIColor.red, UIColor.darkGray], deSelectedTitleColor: UIColor? = UIColor.lightGray, indicatorColors: [UIColor]? = [UIColor.red, UIColor.black], hasRedDot: [Bool]? = nil, segmentButtonFontSize: CGFloat? = 14, navigateToTabIndex: Int? = 0, segmentButtonWidth: Int? = 60) {
+    init(enableSegmentControl: Bool = true, segmentedTitles: [String]? = ["Red", "Black"], selectedTitleColors: [UIColor]? = [UIColor.red, UIColor.darkGray], deSelectedTitleColor: UIColor? = UIColor.lightGray, indicatorColors: [UIColor]? = [UIColor.red, UIColor.black], hasRedDot: [Bool]? = nil, segmentButtonFontSize: CGFloat? = 14, navigateToTabIndex: Int? = 0, isDynamicTabWidth: Bool = true) {
         self.enableSegmentControl = enableSegmentControl
         self.segmentedTitles = segmentedTitles!
         self.selectedTitleColors = selectedTitleColors!
@@ -27,7 +27,7 @@ public struct SegmentedControlOptions {
         self.indicatorColors = indicatorColors!
         self.hasRedDot = hasRedDot // nil means no red dot
         self.segmentButtonFontSize = segmentButtonFontSize ?? 14
-        self.navigateToTabIndex = navigateToTabIndex ?? 0 /* default 0, assign your index for transtition*/
-        self.segmentButtonWidth = segmentButtonWidth
+        self.navigateToTabIndex = navigateToTabIndex ?? 0
+        self.isDynamicTabWidth = isDynamicTabWidth
     }
 }
