ce23ab08253acd252a4ecfea4e9477ae6aab0efc
Kam <>
2019-12-21 21:30:50 +0800

    move customise logic to initializer

--- PageViewControllerExample/ExampleViewController.swift
+++ PageViewControllerExample/ExampleViewController.swift
@@ -13,19 +13,6 @@ class ExampleViewController: PageViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "KPageViewController"
-        // Do any additional setup after loading the view.
         self.navigationController?.navigationBar.isTranslucent = false
-        self.dynamicWidthTab = true
-        self.segmentedTitles = ["Tab", "Tab With Long Title", "Tab", "Tab Tab", "Tab Showing in Half"]
-        
-        let colors = [UIColor.red, UIColor.blue, UIColor.green, UIColor.yellow, UIColor.purple]
-        var vcs: [UIViewController] = []
-        for color in colors {
-            let vc = UIViewController()
-            vc.view.backgroundColor = color
-            vcs.append(vc)
-        }
-        self.viewControllers = vcs
-        self.reveal()
     }
 }
